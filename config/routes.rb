11f57d9256d2c8720e493dd16c6136bb93d138ce
baldor <anuragverma65@gmail.com>
2016-03-21 11:39:39 +0530

    added bootstrap in the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   get 'bookings/index'
 
   get 'holidays/index'
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 	resources :rooms do
     resources :bookings
   end
